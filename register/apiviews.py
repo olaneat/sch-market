ffbb548d69eb999d8b9c392b7acd04c759fa8e2a
Tosin Ayoola <tosinayoola0@gmail.com>
2021-05-20 00:14:09 +0100

    chamges

--- register/apiviews.py
+++ register/apiviews.py
@@ -34,6 +34,13 @@ class RegistrationAPIView(APIView):
         )
 
 
+class CreateProfileView(generics.CreateAPIView):
+    parser_classes = (MultiPartParser,)
+    serializer_class = schoolProfileSerializer
+    queryset = Profile.objects.all()
+    permission_classes = [permissions.AllowAny]
+
+
 class LoginAPIView(APIView):
     permission_classes = [AllowAny]
     serializer_class = LoginSerializer
--- schProfile/serializers.py
+++ schProfile/serializers.py
@@ -11,58 +11,60 @@ from register.models import CustomUser
 
 
 class Base64Imagefield(serializers.ImageField):
-  
-  def to_internal_value(self, data):
-    if isinstance(data, six.string_types):
-      if 'data:' in data and ';base64,' in data:
-        header, data = data.split(';base64,')
-      try:
-        decoded_file = base64.b64decode(data)
-      except TypeError:
-        self.fail('invalid_image')
 
-      file_name = str(uuid.uuid4())[:12]
-      file_extension = self.get_file_extension(file_name, decoded_file)
-      complete_file_name = "%s.%s" %(file_name, file_extension)
-      data = ContentFile(decoded_file, name=complete_file_name)
-    return super(Base64Imagefield, self).to_internal_value(data)  
+    def to_internal_value(self, data):
+        if isinstance(data, six.string_types):
+            if 'data:' in data and ';base64,' in data:
+                header, data = data.split(';base64,')
+            try:
+                decoded_file = base64.b64decode(data)
+            except TypeError:
+                self.fail('invalid_image')
 
-  def get_file_extension(self, file_name, decoded_file):
-    extension = imghdr.what(file_name, decoded_file)
-    extension = 'jpg' if extension == 'jpeg' else extension
+            file_name = str(uuid.uuid4())[:12]
+            file_extension = self.get_file_extension(file_name, decoded_file)
+            complete_file_name = "%s.%s" % (file_name, file_extension)
+            data = ContentFile(decoded_file, name=complete_file_name)
+        return super(Base64Imagefield, self).to_internal_value(data)
 
-    return extension
+    def get_file_extension(self, file_name, decoded_file):
+        extension = imghdr.what(file_name, decoded_file)
+        extension = 'jpg' if extension == 'jpeg' else extension
+
+        return extension
+
 
 class schoolProfileSerializer(serializers.ModelSerializer):
-  parser_classes = (MultiPartParser, FormParser, )
-  id = serializers.IntegerField(source='pk', read_only=True)
-  email = serializers.CharField(source='user.email', read_only=True)
-  username = serializers.CharField(source='user.username', read_only=True)
-  badge = Base64Imagefield(max_length=None, use_url=True)
-  date_established = serializers.DateField(format=None,input_formats=None)
-  
+    parser_classes = (MultiPartParser, FormParser, )
+    id = serializers.IntegerField(source='pk', read_only=True)
+    email = serializers.CharField(source='user.email', read_only=True)
+    username = serializers.CharField(source='user.username', read_only=True)
+    badge = Base64Imagefield(max_length=None, use_url=True)
+    date_established = serializers.DateField(format=None, input_formats=None)
 
-  class Meta:
-    model = Profile
-    fields = ( 'email', 'id', 'username', 'school_name',
-              'address', 'badge', 'gender', 'level',
-              'state', 'curriculum', 'extra_curriculum_activities',
-              'website', 'clubs', 'school_phone_number', 'school_type',
-              'school_email', 'school_facilities', 'awards_won', 'date_established',
-              'school_fees_range', 'motto'
-    )
+    class Meta:
+        model = Profile
+        fields = ('email', 'id', 'username', 'school_name',
+                  'address', 'badge', 'gender', 'level',
+                  'state', 'curriculum', 'extra_curriculum_activities',
+                  'website', 'clubs', 'school_phone_number', 'school_type',
+                  'school_email', 'school_facilities', 'awards_won', 'date_established',
+                  'school_fees_range', 'motto'
+                  )
 
-  '''def create(self, validated_data, instance=None):
-    if 'user' in validated_data:
-      user = validated_data.pop('user')
-    else:
-      user = CustomUser.objects.create(**validated_data)
-    profile, created_profile = schoolProfile.objects.update_or_create(user=user, **validated_data)
-    return profile
-'''
+    def create(self, validated_data, instance=None):
+        if 'user' in validated_data:
+            user = validated_data.pop('user')
+        else:
+            user = CustomUser.objects.create(**validated_data)
+        profile, created_profile = schoolProfile.objects.update_or_create(
+            user=user, **validated_data)
+        return profile
 
-def get_username(self,obj):
-  return obj.user.username
+
+def get_username(self, obj):
+    return obj.user.username
+
 
 def get_email(self, obj):
-  return obj.user.email
+    return obj.user.email
