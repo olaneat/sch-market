5bbf130e4fd6f3bfe3c82839ac9be5dba35e1147
Mfon Eti-mfon <mfonetimfon@gmail.com>
2021-05-19 13:29:03 +0100

    Work on apiviews and serializers in schProfile

--- schProfile/apiviews.py
+++ schProfile/apiviews.py
@@ -3,56 +3,60 @@ from .models import Profile
 from rest_framework import permissions
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.views import APIView
 from .serializers import schoolProfileSerializer
-from rest_framework.parsers import FileUploadParser, MultiPartParser, FormParser
+from rest_framework.parsers import MultiPartParser, FormParser
+
 
 class CreateProfileView(generics.CreateAPIView):
-  parser_classes = (MultiPartParser, FormParser)
-  serializer_class = schoolProfileSerializer
-  queryset = Profile.objects.all()
-  permission_classes = [permissions.AllowAny]
+    parser_classes = (MultiPartParser, FormParser)
+    serializer_class = schoolProfileSerializer
+    queryset = Profile.objects.all()
+    permission_classes = [permissions.AllowAny]
 
- 
-   
-  def perform_create(self, serializer):
-    serializer.save(user=self.request.user)
+    def post(self, request):
+        file_upload = schoolProfileSerializer(data=request.data, instance=request.user)
 
-  def post(self, request):
-    file_upload = schoolProfileSerializer(data =request.data, instance=request.user)
-    if  file_upload.is_valid():
-      return Response(file_upload.data, status=status.HTTP_201_CREATED)
-      file_upload.save()
-      return super().post(request)
-    return Response(file_upload.errors, status=status.HTTP_400_BAD_REQUEST)
-  
+        if not file_upload.is_valid():
+            return Response(file_upload.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        file_upload.save()
+        return super().post(request)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
 
 class DisplaySchoolList(generics.ListAPIView):
-  serializer_class = schoolProfileSerializer
-  queryset = Profile.objects.all()
-  permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = schoolProfileSerializer
+    queryset = Profile.objects.all()
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
 
 class DispaySchoolDetail(generics.RetrieveAPIView):
-  permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-  serializer_class = schoolProfileSerializer
-  queryset = Profile.objects.all()
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = schoolProfileSerializer
+    queryset = Profile.objects.all()
+
 
 class UpdateSchoolProfile(generics.UpdateAPIView):
-  parser_classes = (MultiPartParser, FormParser,)
-  permission_classes = [permissions.IsAuthenticated]
-  queryset = Profile.objects.all()
-  serializer_class = schoolProfileSerializer
+    parser_classes = (
+        MultiPartParser,
+        FormParser,
+    )
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = Profile.objects.all()
+    serializer_class = schoolProfileSerializer
 
-  def post(self, request):
-    badge = schoolProfileSerializer(data=request.data)
-    if badge.is_valid():
-      badge.save()
-      return Response(badge.data, status=status.HTTP_200_OK)
-    else:
-      return Response(badge.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request):
+        badge = schoolProfileSerializer(data=request.data)
+        if badge.is_valid():
+            badge.save()
+            return Response(badge.data, status=status.HTTP_200_OK)
+        else:
+            return Response(badge.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class DeleteSchoolProfile(generics.DestroyAPIView):
-  permission_classes = [permissions.IsAuthenticated]
-  serializer_class = schoolProfileSerializer
-  queryset = Profile.objects.all()
-  
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = schoolProfileSerializer
+    queryset = Profile.objects.all()
--- schProfile/serializers.py
+++ schProfile/serializers.py
@@ -1,24 +1,25 @@
-from rest_framework.parsers import FileUploadParser, MultiPartParser, FormParser
+from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework import serializers
-#from django.core.files.base import ContentFile
-from django.conf import settings
+
+from django.core.files.base import ContentFile
 import base64
 import imghdr
 import six
 import uuid
 from .models import Profile
+
 from register.models import CustomUser
 
 
 class Base64Imagefield(serializers.ImageField):
     def to_internal_value(self, data):
         if isinstance(data, six.string_types):
-            if 'data:' in data and ';base64,' in data:
-                header, data = data.split(';base64,')
+            if "data:" in data and ";base64," in data:
+                header, data = data.split(";base64,")
             try:
                 decoded_file = base64.b64decode(data)
             except TypeError:
-                self.fail('invalid_image')
+                self.fail("invalid_image")
 
             file_name = str(uuid.uuid4())[:12]
             file_extension = self.get_file_extension(file_name, decoded_file)
@@ -28,61 +29,59 @@ class Base64Imagefield(serializers.ImageField):
 
     def get_file_extension(self, file_name, decoded_file):
         extension = imghdr.what(file_name, decoded_file)
-        extension = 'jpg' if extension == 'jpeg' else extension
+        extension = "jpg" if extension == "jpeg" else extension
         return extension
 
 
 class schoolProfileSerializer(serializers.ModelSerializer):
-    ##id = serializers.IntegerField(source='pk', read_only=True)
-    parser_classes = (MultiPartParser, FormParser, )
-    email = serializers.CharField(source='user.email', read_only=True)
-    username = serializers.CharField(source='user.username', read_only=True)
+    id = serializers.IntegerField(source="pk", read_only=True)
+    parser_classes = (
+        MultiPartParser,
+        FormParser,
+    )
+    email = serializers.CharField(source="user.email", read_only=True)
+    username = serializers.CharField(source="user.username", read_only=True)
     badge = Base64Imagefield(max_length=None, use_url=True)
     date_established = serializers.DateField(format=None, input_formats=None)
 
     class Meta:
         model = Profile
-        fields = ('email', 'id', 'username', 'school_name',
-                  'address', 'badge', 'gender', 'level',
-                  'state', 'curriculum', 'extra_curriculum_activities',
-                  'website', 'clubs', 'school_phone_number', 'school_type',
-                  'school_email', 'school_facilities', 'awards_won', 'date_established',
-                  'school_fees_range', 'motto'
-                  )
-
-    '''
-    
-  def create(self, validated_data):
-    if 'profile' in validated_data:
-      user_data = validated_data.pop('profile')
-    user = CustomUser.objects._create_user(**validated_data)
-    Profile.objects.update_or_create(user=user, **validated_data)
-    return user
-
+        fields = (
+            "email",
+            "id",
+            "username",
+            "school_name",
+            "address",
+            "badge",
+            "gender",
+            "level",
+            "state",
+            "curriculum",
+            "extra_curriculum_activities",
+            "website",
+            "clubs",
+            "school_phone_number",
+            "school_type",
+            "school_email",
+            "school_facilities",
+            "awards_won",
+            "date_established",
+            "school_fees_range",
+            "motto",
+        )
 
     def create(self, validated_data, instance=None):
-    user_data = validated_data.pop('user')
-    user = RegistrationSerializer.create(RegistrationSerializer(), validated_data=user_data)
-    profile, creted = schoolProfileSerializer.update_or_create(user=user, 
-      school_name = validated_data.pop('school_name'),
-      address = validated_data.pop('address'),
-      state = validated_data.pop('state'),
-      school_fees_range = validated_data.pop('school_fees_range'),
-      motto = validated_data.pop('motto'),
-      school_facilities = validated_data.pop('school_facilities'),
-      awards_won = validated_data.pop('awards_won'),
-      date_established = validated_data.pop('date_esablished'),
-      school_type=validated_data.pop("school_type") 
+        if "user" in validated_data:
+            user = validated_data.pop("user")
+        else:
+            user = CustomUser.objects.create(**validated_data)
+        profile, create_profile = Profile.objects.update_or_create(
+            user=user, defaults=validated_data
+        )
+        return profile
 
-    )
-    
-  
-  '''
+    def get_username(self, obj):
+        return obj.user.username
 
-
-def get_username(self, obj):
-    return obj.user.username
-
-
-def get_email(self, obj):
-    return obj.user.email
+    def get_email(self, obj):
+        return obj.user.email
